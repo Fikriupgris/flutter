1caafbec2088f225c781695b051f7a7ce44c0aad
Fikriupgris <137605532+Fikriupgris@users.noreply.github.com>
2023-06-24 15:38:44 +0700

    First Project

---
+++ main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:main/db_provider.dart';
+import 'package:provider/provider.dart';
+import 'note_list_page.dart';
+
+void main() {
+  Provider.debugCheckInvalidValueType = null;
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => DbProvider(),
+        )
+      ],
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const NoteListPage(),
+    );
+  }
+}
